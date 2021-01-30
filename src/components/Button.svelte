681712f61f7410006b09e8e39fdf91768c4c683a
ubaidillahhf <ubaidillahhf@gmail.com>
2021-01-30 09:48:36 +0700

    Init

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,57 +1,64 @@
 <script lang="ts">
-	export let href: string;
-	export let buttonRow: boolean = false;
-	export let primary: boolean = false;
-	export let targetBlank: boolean = false;
+  export let href: string;
+  export let buttonRow: boolean = false;
+  export let primary: boolean = false;
+  export let targetBlank: boolean = false;
 
-	$: buttonRowClass = buttonRow ? "button-row" : "";
-	$: primarySecondaryClass = primary ? "primary" : "secondary";
-	$: targetBlankAttribute = targetBlank ? "_blank" : "_self";
+  $: buttonRowClass = buttonRow ? "button-row" : "";
+  $: primarySecondaryClass = primary ? "primary" : "secondary";
+  $: targetBlankAttribute = targetBlank ? "_blank" : "_self";
 </script>
 
 <!-- Event forward on:click -->
-<a href="{href}" class="{buttonRowClass} {primarySecondaryClass}" target={targetBlankAttribute} on:click><slot/></a>
+<a
+  {href}
+  class="{buttonRowClass} {primarySecondaryClass}"
+  target={targetBlankAttribute}
+  on:click><slot /></a
+>
 
 <style lang="scss">
-	@import "../styles/constants.scss";
+  @import "../styles/constants.scss";
 
-	// Inspired by https://stripe.com
+  // Inspired by https://stripe.com
 
-	a {
-		color: white;
-		white-space: nowrap;
-		display: inline-block;
-		height: 40px;
-		line-height: 40px;
-		padding: 0 14px;
-		-webkit-box-shadow: 0 4px 6px rgba(50,50,93,.11),0 1px 3px rgba(0,0,0,.08);
-		box-shadow: 0 4px 6px rgba(50,50,93,.11),0 1px 3px rgba(0,0,0,.08);
-		background: #fff;
-		background-color: rgb(255, 255, 255);
-		border-radius: 4px;
-		font-size: 15px;
-		font-weight: 600;
-		text-transform: uppercase;
-		letter-spacing: .025em;
-		text-decoration: none;
-		-webkit-transition: all .15s ease;
-		transition: all .15s ease;
+  a {
+    color: white;
+    white-space: nowrap;
+    display: inline-block;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 14px;
+    -webkit-box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11),
+      0 1px 3px rgba(0, 0, 0, 0.08);
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    background: #fff;
+    background-color: rgb(255, 255, 255);
+    border-radius: 4px;
+    font-size: 15px;
+    font-weight: 600;
+    text-transform: uppercase;
+    letter-spacing: 0.025em;
+    text-decoration: none;
+    -webkit-transition: all 0.15s ease;
+    transition: all 0.15s ease;
 
-		&.button-row {
-			margin: 15px 12px 0 0;
-		}
+    &.button-row {
+      margin: 15px 12px 0 0;
+    }
 
-		&.primary {
-			background-color: $logo-colour;
-		}
+    &.primary {
+      background-color: #3eb489;
+    }
 
-		&.secondary {
-			background-color: #32325d;
-		}
+    &.secondary {
+      background-color: #32325d;
+    }
 
-		&:hover {
-			box-shadow: 0 7px 14px rgba(50, 50, 93, .1), 0 3px 6px rgba(0, 0, 0, .08);
-			transform: translateY(-2px);
-		}
-	}
+    &:hover {
+      box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1),
+        0 3px 6px rgba(0, 0, 0, 0.08);
+      transform: translateY(-2px);
+    }
+  }
 </style>
--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,104 +1,110 @@
 <script lang="ts">
-    import Button from './Button.svelte';
-    import NavBarButton from './NavBarButton.svelte';
-    import Section from "./Section.svelte";
+  import Button from "./Button.svelte";
+  import NavBarButton from "./NavBarButton.svelte";
+  import Section from "./Section.svelte";
 
-    // Must link to # for scoll animation
-	// Clearing hash allows user to scroll to /# multiple times in a row
-	let eraseHash = () => {
-		setTimeout(() => {
-			if (history.pushState) {
-				history.pushState(null, null, '/');
-			} else {
-				location.hash = '/';
-			}
-		}, 50);
-	}
+  // Must link to # for scoll animation
+  // Clearing hash allows user to scroll to /# multiple times in a row
+  let eraseHash = () => {
+    setTimeout(() => {
+      if (history.pushState) {
+        history.pushState(null, null, "/");
+      } else {
+        location.hash = "/";
+      }
+    }, 50);
+  };
 </script>
 
 <nav>
-    <Section>
-        <div class="horizontal">
-            <a href="/#" id="logo" on:click={eraseHash}>
-                <img src="/logo.svg" alt="Dennis Krasnov Logo"/>
-                <span>Dennis Krasnov</span>
-            </a>
+  <Section>
+    <div class="horizontal">
+      <a href="/#" id="logo" on:click={eraseHash}>
+        <img src="/logoq.png" alt="Ubed Dev Logo" />
+        <span>Ubed [ DEV ]</span>
+      </a>
 
-            <div class="expand" />
+      <div class="expand" />
 
-            <div class="scroll-links">
-                <NavBarButton hash="skills">skills</NavBarButton>
-                <NavBarButton hash="work">work</NavBarButton>
-                <NavBarButton hash="projects">projects</NavBarButton>
-<!--                <NavBarButton hash="contact">contact</NavBarButton>-->
-            </div>
-            
-            <span class="resume-download">
-                <Button href="/dennis_krasnov_resume.pdf" targetBlank>download resume</Button>
-            </span>
-        </div>
-    </Section>
+      <div class="scroll-links">
+        <NavBarButton hash="skills">Skills</NavBarButton>
+        <NavBarButton hash="work">Work</NavBarButton>
+        <NavBarButton hash="projects">Projects</NavBarButton>
+        <!--                <NavBarButton hash="contact">contact</NavBarButton>-->
+      </div>
+
+      <span class="resume-download">
+        <Button href="/dennis_krasnov_resume.pdf" targetBlank
+          >download resume</Button
+        >
+      </span>
+    </div>
+  </Section>
 </nav>
 
 <style lang="scss">
-    @import "../styles/constants.scss";
+  @import "../styles/constants.scss";
 
-    nav {
-        height: $menu-height;
+  nav {
+    height: $menu-height;
 
-        // Stick to top of screen
-        position: sticky;
-		top: 0;
-        z-index: 3;
-        
-        background: white;
-        
-        .horizontal {
-            display: flex;
-            flex-flow: row nowrap;
-            align-items: center;
-            height: $menu-height;
+    // Stick to top of screen
+    position: sticky;
+    top: 0;
+    z-index: 3;
 
-            a {
-                display: flex;
-                align-items: center;
+    background: white;
 
-                img {
-                    height: 40px;
-                }
+    .horizontal {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      height: $menu-height;
 
-                span {
-                    margin-left: 8px;
-                    font-size: 32px;
-                    font-weight: 600;
-                    white-space: nowrap;
+      a {
+        display: flex;
+        align-items: center;
 
-                    color: #343434; // Same as logo
-                }
-            }
-
-            .expand {
-                flex: 1;
-            }
-
-            .scroll-links {
-                display: flex;
-                flex-flow: row nowrap;
-				justify-content: flex-end;
-				align-items: center;
-
-                margin: 0 20px 0 0;
-                
-                @media (max-width: $landing-page-breakpoint) {& {
-                    display: none;
-                }}
-            }
-
-            .resume-download {
-                @media (max-width: 500px) {& {
-                    display: none;
-                }}
-            }
+        img {
+          height: 40px;
         }
+
+        span {
+          margin-left: 8px;
+          font-size: 32px;
+          font-weight: 600;
+          white-space: nowrap;
+
+          color: #3eb489; // Same as logo
+        }
+      }
+
+      .expand {
+        flex: 1;
+      }
+
+      .scroll-links {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-end;
+        align-items: center;
+
+        margin: 0 20px 0 0;
+
+        @media (max-width: $landing-page-breakpoint) {
+          & {
+            display: none;
+          }
+        }
+      }
+
+      .resume-download {
+        @media (max-width: 500px) {
+          & {
+            display: none;
+          }
+        }
+      }
     }
+  }
 </style>
