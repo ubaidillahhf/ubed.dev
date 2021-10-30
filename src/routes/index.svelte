fa5c77aaec360a219f594c9fc08fbe70aedf3769
ubaidillah-hakim <ubaidillah.hakim@sinbad.co.id>
2021-10-30 20:09:28 +0700

    kk

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,7 +34,7 @@
 <Section>
   <Landing
     title="Experienced"
-    summary="Full stack software engineer with {yearsExperience} years experience, Specialized for NodeJS & Hybrid React Native."
+    summary="Wong Biasa {yearsExperience} years experience, Specialized for NodeJS & Hybrid React Native."
     imageUrl="/caricature.png"
   >
     <span slot="cta">
